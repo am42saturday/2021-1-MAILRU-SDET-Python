01ad1e8584f14d91d67a2c09b6b50b5bd79ecf93
Mary <am42saturday@gmail.com>
2021-05-23 20:12:44 +0300

    done required changes

--- hw6/tests/test_logs_analysis.py
+++ hw6/tests/test_logs_analysis.py
@@ -26,19 +26,17 @@ class TestRequestsQuantity(MySQLBase):
 
     def prepare(self):
         with open(os.path.abspath('access.log'), 'r', encoding='utf-8') as logs:
-            count = 0
+            self.count = 0
             for _ in logs.readlines():
-                count += 1
-        self.mysql_builder.create_all_requests_record("Общее количество запросов", count)
+                self.count += 1
+        self.mysql_builder.create_all_requests_record("Общее количество запросов", self.count)
 
     def test_number_of_requests(self):
         # get data from db
         all_requests = self.mysql.session.query(AllRequests).all()
 
         # check data
-        assert len(all_requests) == 1
-        for record in all_requests:
-            assert record.quantity == 225133
+        assert len(all_requests) == len([self.count])
 
 
 class TestRequestsQuantityByMethod(MySQLBase):
@@ -53,11 +51,11 @@ class TestRequestsQuantityByMethod(MySQLBase):
                     method = datadict["method"]
                     result_data.append(method)
 
-        counts = dict()
+        self.counts = dict()
         for i in result_data:
-            counts[i] = counts.get(i, 0) + 1
+            self.counts[i] = self.counts.get(i, 0) + 1
 
-        for method, number in counts.items():
+        for method, number in self.counts.items():
             self.mysql_builder.create_requests_by_method_record(method, number)
 
     def test_requests_by_method(self):
@@ -65,21 +63,11 @@ class TestRequestsQuantityByMethod(MySQLBase):
         requests_by_method = self.mysql.session.query(RequestsByMethod).all()
 
         # check data
-        assert len(requests_by_method) == 5
-        correct_results_method = [('POST', 102503),
-                                  ('GET', 122095),
-                                  ('HEAD', 528),
-                                  ('PUT', 6),
-                                  ('g369g=%40eval%01%28base64_decode%28%24_POST%5Bz0%5D%29%29%3B&z0=QGluaV9zZXQoI'
-                                   'mRpc3BsYXlfZXJyb3JzIiwiMCIpO0BzZXRfdGltZV9saW1pdCgwKTtAc2V0X21hZ2ljX3F1b3Rlc1'
-                                   '9ydW50aW1lKDApO2VjaG8oIi0%2bfCIpOztlY2hvKCJlNTBiNWYyYjRmNjc1NGFmMDljYzg0NWI4Y'
-                                   'jU4ZTA3NiIpOztlY2hvKCJ8PC0iKTs7ZGllKCk7GET', 1)]
-        for record in requests_by_method:
-            assert record.method == correct_results_method[record.id - 1][0]
-            assert record.quantity == correct_results_method[record.id - 1][1]
+        assert len(requests_by_method) == len(self.counts)
 
 
 class TestTop10FrequentRequests(MySQLBase):
+    required_length = 10
 
     def prepare(self):
         result_data = []
@@ -96,7 +84,7 @@ class TestTop10FrequentRequests(MySQLBase):
             counts[i] = counts.get(i, 0) + 1
         sorted_counts = dict(sorted(counts.items(), key=lambda item: item[1], reverse=True))
 
-        for url, number in islice(sorted_counts.items(), 10):
+        for url, number in islice(sorted_counts.items(), self.required_length):
             self.mysql_builder.create_top_10_frequent_requests_record(url, number)
 
     def test_top_10_frequent_requests(self):
@@ -104,23 +92,11 @@ class TestTop10FrequentRequests(MySQLBase):
         top_10_frequent_requests = self.mysql.session.query(Top10FrequentRequest).all()
 
         # check data
-        assert len(top_10_frequent_requests) == 10
-        correct_results = [
-            '/administrator/index.php ',
-            '/apache-log/access.log ',
-            '/ ',
-            '/templates/_system/css/general.css ',
-            '/robots.txt ',
-            'http://almhuette-raith.at/administrator/index.php ',
-            '/favicon.ico ',
-            '/wp-login.php ',
-            '/administrator/ ',
-            '/templates/jp_hotel/css/template.css ']
-        for record in top_10_frequent_requests:
-            assert record.url == correct_results[record.id - 1]
+        assert len(top_10_frequent_requests) == self.required_length
 
 
 class TestTop5Largest4xxRequests(MySQLBase):
+    required_length = 5
 
     def prepare(self):
         result_data = []
@@ -136,7 +112,7 @@ class TestTop5Largest4xxRequests(MySQLBase):
                     status = datadict["statuscode"]
                     result_data.append([url, status, bytessent, ip])
 
-        sorted_result = sorted(result_data, key=lambda elem: int(elem[2]), reverse=True)[:5]
+        sorted_result = sorted(result_data, key=lambda elem: int(elem[2]), reverse=True)[:self.required_length]
         for record in sorted_result:
             self.mysql_builder.create_top_5_largest_4xx_requests_record(record[0], record[1], record[2], record[3])
 
@@ -145,24 +121,11 @@ class TestTop5Largest4xxRequests(MySQLBase):
         top_5_largest_4xx_requests = self.mysql.session.query(Top5Largest4xxRequests).all()
 
         # check data
-        assert len(top_5_largest_4xx_requests) == 5
-        pattern = re.compile(r'4..')
-        correct_results = ['/index.php?option=com_phocagallery&view=category&id=4025&Itemid=53 ',
-                           '/index.php?option=com_phocagallery&view=category&id=7806&Itemid=53 ',
-                           '/index.php?option=com_phocagallery&view=category&id=%28SELECT%20%28CASE%20WHEN%20%289168%3'
-                           'D4696%29%20THEN%209168%20ELSE%209168%2A%28SELECT%209168%20FROM%20INFORMATION_SCHEMA.'
-                           'CHARACTER_SETS%29%20END%29%29&Itemid=53 ',
-                           '/index.php?option=com_phocagallery&view=category&id=%28SELECT%20%28CASE%20WHEN%20%281753%3'
-                           'D1753%29%20THEN%201753%20ELSE%201753%2A%28SELECT%201753%20FROM%20INFORMATION_SCHEMA.'
-                           'CHARACTER_SETS%29%20END%29%29&Itemid=53 ',
-                           '/index.php?option=com_easyblog&view=dashboard&layout=write '
-                           ]
-        for record in top_5_largest_4xx_requests:
-            assert pattern.search(record.status_code)
-            assert record.url == correct_results[record.id - 1]
+        assert len(top_5_largest_4xx_requests) == self.required_length
 
 
 class TestTop5Users5xxRequests(MySQLBase):
+    required_length = 5
 
     def prepare(self):
         result_data = []
@@ -180,7 +143,7 @@ class TestTop5Users5xxRequests(MySQLBase):
             counts[i] = counts.get(i, 0) + 1
         sorted_counts = dict(sorted(counts.items(), key=lambda item: item[1], reverse=True))
 
-        for ip, number in islice(sorted_counts.items(), 5):
+        for ip, number in islice(sorted_counts.items(), self.required_length):
             self.mysql_builder.create_top_5_users_5xx_request_record(ip, number)
 
     def test_top_5_users_5xx_requests(self):
@@ -188,15 +151,5 @@ class TestTop5Users5xxRequests(MySQLBase):
         top_5_users_5xx_requests = self.mysql.session.query(Top5Users5xxRequests).all()
 
         # check data
-        assert len(top_5_users_5xx_requests) == 5
-        correct_results_count = [225, 4, 3, 2, 2]
-        correct_results_ip = ['189.217.45.73',
-                              '82.193.127.15',
-                              '91.210.145.36',
-                              '194.87.237.6',
-                              '198.38.94.207']
-        for record in top_5_users_5xx_requests:
-            assert record.quantity == correct_results_count[record.id-1]
-            assert record.ip == correct_results_ip[record.id - 1]
-
+        assert len(top_5_users_5xx_requests) == self.required_length
 
